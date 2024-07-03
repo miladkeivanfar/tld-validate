935c0cbfc4f3774767fe85558f3e49a87dff003e
Milad Keivanfar <129506375+miladkeivanfar@users.noreply.github.com>
2024-07-03 17:08:52 +0330

    Update tld_validate.py
    fix exclude tld

--- tld_validate.py
+++ tld_validate.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import argparse
+import os
 from colorama import Fore, Style
 from tabulate import tabulate
 
@@ -17,24 +18,32 @@ def report(valid_domains, invalid_domains):
     print(tabulate(table, headers=["Name", "Quantity",], tablefmt="github"))
 
 
-def validate_domains(domains, valid_tlds):
-
+def validate_domains(domains, valid_tlds, exclude=""):
     valid_domains = []
     invalid_domains = []
+    exclude_tld = exclude
     valid_tlds_set = set(valid_tlds)
     for domain in domains:
         if any(domain.endswith(tld) for tld in valid_tlds_set):
-            valid_domains.append(domain)
+            if exclude_tld != None:
+                if domain.endswith(exclude_tld):
+                    invalid_domains.append(domain)
+                else:
+                    valid_domains.append(domain)
+            else:
+                valid_domains.append(domain)
         else:
             invalid_domains.append(domain)
-    return valid_domains,invalid_domains
+    return valid_domains, invalid_domains
 
 def main():
     parser = argparse.ArgumentParser(description='TLD validation')
     parser.add_argument("-df", "--domains", help="domains file", required=True)
     parser.add_argument("-tf", "--validtld", help="valid TLD file", required=False)
     parser.add_argument("-in", "--invaliddomains", help="invalid domains list in result",action="store_true", required=False)
-    parser.add_argument("-s", "--silent", help="silent report",action="store_true", required=False)
+    parser.add_argument("-ex", "--exclude", help="exclude tld", required=False)
+    parser.add_argument("-s", "--silent", help="silent report", action="store_true", required=False)
+
 
     args = parser.parse_args()
 
@@ -43,16 +52,17 @@ def main():
             with open(args.domains, "r") as file:
                 domains = [domain.strip() for domain in file.readlines()]
         else:
-            print("Error: Domain file Does not exist or is not readable.")
+            print("Error: Domains file Does not exist or is not readable.")
 
         if args.validtld:
             with open(args.validtld, "r") as file:
                 valid_tlds = [tld.strip() for tld in file.readlines()]
         else:
             with open("valid_tld.txt", "r") as file:
-                valid_tlds = [tld.strip() for tld in file.readlines()]           
+                valid_tlds = [tld.strip() for tld in file.readlines()]  
+        
+        valid_domains, invalid_domains = validate_domains(domains, valid_tlds,args.exclude)
 
-        valid_domains, invalid_domains = validate_domains(domains, valid_tlds)
 
         if args.invaliddomains:
             for domain in invalid_domains:
